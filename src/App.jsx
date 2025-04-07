5548ba871b6dc42af803977396a4981b21db7b6d
Ayush701-code-zm <h3yayush@gmail.com>
2025-04-08 00:28:01 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Ha Ji GoodMorning</h1>
+
+      <p className="read-the-docs">Mera Naam Ayush Hai</p>
+    </>
+  );
+}
+
+export default App;
